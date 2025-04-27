5d23176f5bf3494efccb2ee7d1b645c75d3a0b4c
Jyoti kamble <jyotikamble5235@gmail.com>
2025-04-27 13:13:43 +0530

    alldone

--- memeapp/src/components/MemeDisplay.jsx
+++ memeapp/src/components/MemeDisplay.jsx
@@ -1,36 +1,39 @@
+// here the data is fetched from the API and displayed
+// importing the hooks and react
 import React, { useState, useEffect } from "react";
 
+// function to display the memes
 function MemeDisplay() {
     const [meme, setMemes] = useState([]);
     const [loading, setLoading] = useState(true);
-    // const[error,setError]=useState(null);
 
-
+    // hook to fetch the memes from the API
     useEffect(() => {
         async function fetchMeme() {
             try {
-
                 const response = await fetch("https://meme-api.com/gimme/50");
                 const data = await response.json();
+                // json data
+
                 console.log(data);
                 setMemes(data.memes);
                 setLoading(false);
+                // set the loading to false when the data is fetched
 
             } catch (error) {
                 console.error("Error fetching memes:", error);
-
             }
         }
-
         fetchMeme();
-
     }, [])
     // query
 
+    // if the data is not fetched then return the loading state
     if (loading) {
         return <div>Loading...</div>;
     }
 
+    // result of the data fetched from the API
     return (
         <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">{meme.map((meme, index) => (
             <div className="bg-white p-4 rounded-lg shadow-lg" key={index}>
@@ -40,7 +43,6 @@ function MemeDisplay() {
             </div>))}
         </div>
     )
-
 }
 
-export default MemeDisplay
+export default MemeDisplay;
--- memeapp/src/main.jsx
+++ memeapp/src/main.jsx
@@ -8,3 +8,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// this from vite itself
